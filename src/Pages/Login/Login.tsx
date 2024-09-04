8700f12e74293615701a8dc4d9226c00a7cdfe79
verstands <rabbymbamu@gmail.com>
2024-09-04 08:31:00 +0100

    demp

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -62,7 +62,6 @@ const Login = () => {
                   </div>
                   <div></div>
                 </div>
-                
                   <button
                     type="submit"
                     className="w-full text-white bg-primary-600 bg-[#0476e8] hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600  dark:focus:ring-primary-800"
